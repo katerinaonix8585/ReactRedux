125ded928c6d90cfcc3b06c05aa0ca06b26a593b
katerinaonix8585 <katerinaonix8585@gmail.com>
2024-05-15 07:37:44 +0200

    add fix

--- src/homeworks/Homework16/Homework16.tsx
+++ src/homeworks/Homework16/Homework16.tsx
@@ -1,10 +1,14 @@
 import Counter from "components/Counter/Counter";
 import { useState } from "react";
+import { Homework16Wrapper } from "./styles";
 
 function Homework16() {    
 
     return (
-        <Counter />
+        <Homework16Wrapper>
+            <Counter />
+        </Homework16Wrapper>
+        
     )
 }
 
